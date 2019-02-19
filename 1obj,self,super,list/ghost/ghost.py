b685061d9661f21183f3a56649efc187a01c7191
roses19860321 <569438940@qq.com>
2019-02-19 12:25:30 +0800

    obj

---
+++ 1obj,self,super,list/ghost/ghost.py
@@ -0,0 +1,15 @@
+class Ghost:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+    def setter(self,name,age):
+        self.name=name
+        self.age=age
+
+    def getName(self):
+        return self.name
+
+    def getAge(self):
+        return self.age
+
